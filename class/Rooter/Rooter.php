a3834665699fea62f3660ae64b34dc03703633f8
Anhgelus <contact@horrorseriesprod.org>
2020-12-10 12:53:11 +0100

    Adding if get (don't try to execute)

--- class/Rooter/Rooter.php
+++ class/Rooter/Rooter.php
@@ -26,9 +26,30 @@ class Rooter {
                 require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'elements' . (string)$uri;
                 return $content = ob_get_clean();
             } else {
-                ob_start();
-                require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'elements/404.php';
-                return $content = ob_get_clean();
+
+                $position = strpos((string)$uri, "?");
+
+                if (!$position) {
+                    ob_start();
+                    require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'elements/404.php';
+                    return $content = ob_get_clean();
+                } else {
+                    $sizeUri = strlen((string)$uri);
+
+                    $uri_TEMP = ($uri - $sizeUri) + 1;
+  
+                    if (file_exists(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'elements' . substr((string)$uri, 0, $uri_TEMP))) {
+                        ob_start();
+                        require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'elements' . (string)$uri;
+                        return $content = ob_get_clean();
+                    } else {
+                        ob_start();
+                        require dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'elements/404.php';
+                        return $content = ob_get_clean();
+                    }
+                }
+
+                
             }
             
         }
@@ -42,20 +63,40 @@ class Rooter {
     {
         $uri = $this->uri;
 
+        function createTitle(string $uri): string {
+            $lenght = strlen($uri) - 5;
+
+            $preTitle = substr($uri, 1, $lenght);
+            $firstLetter = strtoupper(substr($preTitle, 0, 1));
+            
+            $title = $firstLetter . substr($preTitle, 1);
+            return $title;
+        }
+
         if ($uri === '/') { // si on est dans la racine du site
             return 'Accueil';
         } else {
-            if (file_exists(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'elements' . (string)$uri)) {
-                $lenght = strlen($uri) - 5;
 
-                $preTitle = substr($uri, 1, $lenght);
-                $firstLetter = strtoupper(substr($preTitle, 0, 1));
-                
-                $title = $firstLetter . substr($preTitle, 1);
-                return $title;
+            if (file_exists(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'elements' . (string)$uri)) {
+                createTitle($uri);
             } else {
-                return '404';
+                $position = strpos((string)$uri, "?");
+
+                if (!$position) {
+                    return '404';
+                } else {
+                    $sizeUri = strlen((string)$uri);
+
+                    $uri_TEMP = ($uri - $sizeUri) + 1;
+
+                    if (file_exists(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'elements' . substr((string)$uri, 0, $uri_TEMP))) {
+                        createTitle($uri);
+                    } else {
+                        return '404';
+                    }
+                }
             }
+
         }
 
     }
